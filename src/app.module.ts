15b312dee060a1860dbee27e440c37a07d7abb71
GormanProg123 <gparfenchik2005@gmail.com>
2026-05-20 01:43:45 +0200

    second-commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,39 +3,56 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { User } from 'src/infrastructure/database/schemas/user.entity';
 import { Item } from 'src/infrastructure/database/schemas/item.entity';
 import { Tag } from 'src/infrastructure/database/schemas/tag.entity';
 import { Reminder } from 'src/infrastructure/database/schemas/reminder.entity';
+import { PasswordReset } from 'src/infrastructure/database/schemas/password-reset.entity';
+
 import { AuthModule } from 'src/presentation/modules/auth.module';
 import { ItemsModule } from 'src/presentation/modules/item.module';
-import { ScheduleModule } from '@nestjs/schedule';
-import { PasswordReset } from 'src/infrastructure/database/schemas/password-reset.entity';
 import { UserModule } from 'src/presentation/modules/user.module';
+
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
+
+      inject: [ConfigService],
+
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
+
+        url: config.get<string>('DATABASE_URL'),
+
+        ssl: {
+          rejectUnauthorized: false,
+        },
+
         entities: [User, Item, Tag, Reminder, PasswordReset],
+
         synchronize: false,
+
+        autoLoadEntities: true,
       }),
-      inject: [ConfigService],
     }),
+
     AuthModule,
     ItemsModule,
     UserModule,
+
     ScheduleModule.forRoot(),
   ],
+
   controllers: [AppController],
+
   providers: [AppService],
 })
 export class AppModule {}
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -5,11 +5,15 @@ dotenv.config();
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+
+  url: process.env.DATABASE_URL,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   entities: ['src/infrastructure/database/schemas/*.entity.ts'],
   migrations: ['src/infrastructure/database/migrations/*.ts'],
+
+  synchronize: false,
 });
