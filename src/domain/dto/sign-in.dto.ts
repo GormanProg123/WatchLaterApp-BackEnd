8cd9d924d9f6b236cc8e3726d9bab47baab8ff36
GormanProg123 <gparfenchik2005@gmail.com>
2026-05-20 14:08:39 +0200

    [sixth commit]

--- src/domain/dto/sign-in.dto.ts
+++ src/domain/dto/sign-in.dto.ts
@@ -1,9 +1,20 @@
-import { IsEmail, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class SignInDto {
   @IsEmail()
   email!: string;
 
   @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters' })
+  @MaxLength(64, { message: 'Password is too long' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
+    message: 'Password must contain uppercase, lowercase letters and numbers',
+  })
   password!: string;
 }
--- src/domain/dto/sign-up.dto.ts
+++ src/domain/dto/sign-up.dto.ts
@@ -1,11 +1,22 @@
-import { IsEmail, MinLength, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  MinLength,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Matches,
+} from 'class-validator';
 
 export class SignUpDto {
   @IsEmail()
   email!: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(6, { message: 'Password must be at least 6 characters' })
+  @MaxLength(64, { message: 'Password is too long' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
+    message: 'Password must contain uppercase, lowercase letters and numbers',
+  })
   password!: string;
 
   @IsString()
