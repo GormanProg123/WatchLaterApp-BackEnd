369798ef2d82def90937bb79fa82fcb58aa2a8d2
GormanProg123 <gparfenchik2005@gmail.com>
2026-05-20 02:02:41 +0200

    [third-commit]

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { ScheduleModule } from '@nestjs/schedule';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
 
     TypeOrmModule.forRootAsync({
