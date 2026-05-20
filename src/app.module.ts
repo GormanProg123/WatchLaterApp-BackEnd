0463e84cf906dfdbf0a6f130db341e4bd9401de4
GormanProg123 <gparfenchik2005@gmail.com>
2026-05-20 02:54:19 +0200

    fifth commiit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
@@ -56,9 +54,5 @@ import { ScheduleModule } from '@nestjs/schedule';
 
     ScheduleModule.forRoot(),
   ],
-
-  controllers: [AppController],
-
-  providers: [AppService],
 })
 export class AppModule {}
